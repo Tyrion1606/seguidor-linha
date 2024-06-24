c0e4955c7d4a629c92847cbdcbc0ad4e7f51d378
Davi Monteiro Alves <94870055+Tyrion1606@users.noreply.github.com>
2024-06-24 13:48:04 -0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -9,7 +9,7 @@
 #define PINB1 5 // Pino de controle PWM para motor B (esuqerda)
 #define PINB2 4 // Pino de controle que será configurado como LOW (GND) para motor B (esquerda)
 #define NUMPWM 1
-#define PWM 65 // Velocidade PWM desejada
+#define PWM 120 // Velocidade PWM desejada
 
 MX1508 motorA(PINA1, PINA2, FAST_DECAY, NUMPWM);
 MX1508 motorB(PINB1, PINB2, FAST_DECAY, NUMPWM);
@@ -19,16 +19,18 @@ MX1508 motorB(PINB1, PINB2, FAST_DECAY, NUMPWM);
 #define ENCODER_B 3
 
 // Configuração dos Sensores IR
-#define IR_PIN_RIGHT_1 A0
+#define CENTER A0
 #define IR_PIN_RIGHT_2 A1
-#define IR_PIN_LEFT_1 A2
 #define IR_PIN_LEFT_2 A3
-#define threshold 100
+#define threshold 150
 
-bool onLine_r1 = false;
+// bool onLine_r1 = false;
 bool onLine_r2 = false;
-bool onLine_l1 = false;
+// bool onLine_l1 = false;
 bool onLine_l2 = false;
+bool onLine_center = false;
+
+bool turning_FLAG = false;
 
 // Configuração do Amperimetro
 #define ANMETER A5  // Pino analógico onde o sensor está conectado
@@ -37,9 +39,9 @@ bool onLine_l2 = false;
 void setup(){
   Serial.begin(9600);
 
-  pinMode(IR_PIN_RIGHT_1,INPUT);
+  pinMode(CENTER,INPUT);
   pinMode(IR_PIN_RIGHT_2,INPUT);
-  pinMode(IR_PIN_LEFT_1,INPUT);
+  // pinMode(IR_PIN_LEFT_1,INPUT);
   pinMode(IR_PIN_LEFT_2,INPUT);
 
   pinMode(ANMETER, INPUT);
@@ -55,14 +57,55 @@ void setup(){
 }
 void loop (){
   // Set Motor Speed - Walk on line
-  irReadValues();
-  const int motorA_PWM = (((!onLine_r1 && !onLine_r2)*PWM)+((onLine_r2)*3*-PWM));
-  const int motorB_PWM = (((!onLine_l1 && !onLine_l2)*PWM)+((onLine_l2)*3*-PWM));
-  motorA.motorGo(motorA_PWM);
-  motorB.motorGo(motorB_PWM);
-  if(onLine_r2 || onLine_l2){
-    delay(50);
+  if(!turning_FLAG){
+    onLine_r2 = (analogRead(IR_PIN_RIGHT_2) > threshold);
+    onLine_l2 = (analogRead(IR_PIN_LEFT_2) > threshold);
+
+    // const int motorA_PWM = ((!onLine_r2)*PWM);
+    // const int motorB_PWM = ((!onLine_l2)*PWM);
+    motorA.motorGo((!onLine_r2)*PWM);
+    motorB.motorGo((!onLine_l2)*PWM);
+    turning_FLAG = (onLine_r2 || onLine_l2);
+    if (turning_FLAG){
+      if (onLine_r2){
+        motorA.motorGo(-PWM);
+      }
+      if (onLine_l2){
+        motorB.motorGo(-PWM);
+      }
+    }
   }
+  if(turning_FLAG){
+    onLine_center = (analogRead(CENTER) > threshold);
+    if(onLine_center){
+      onLine_r2 = (analogRead(IR_PIN_RIGHT_2) > threshold);
+      onLine_l2 = (analogRead(IR_PIN_LEFT_2) > threshold);
+      motorA.motorGo((!onLine_r2)*PWM);
+      motorB.motorGo((!onLine_l2)*PWM);
+      turning_FLAG = false;
+      onLine_center = false;
+    }
+  }
+
+  Serial.print(onLine_l2);
+  Serial.print(onLine_center);
+  Serial.print(onLine_r2);
+  Serial.print(" - ");
+  Serial.println(turning_FLAG);
+
+
+
+  // if( && (onLine_r2 || onLine_l2)){
+  //   turning_FLAG = true;
+  //   const int motorA_PWM = ((!onLine_r2)*PWM);
+  //   const int motorB_PWM = ((!onLine_l2)*PWM);
+  // }
+
+  // const int motorA_PWM = (((!onLine_r1 && !onLine_r2)*PWM)+((onLine_r2)*3*-PWM));
+  // const int motorB_PWM = (((!onLine_l1 && !onLine_l2)*PWM)+((onLine_l2)*3*-PWM));
+  // motorA.motorGo(motorA_PWM);
+  // motorB.motorGo(motorB_PWM);
+
   // motorB.motorGo(0);
 
   // Consumo
@@ -79,10 +122,11 @@ void loop (){
 
 
 void irReadValues() {
-  onLine_r1 = (analogRead(IR_PIN_RIGHT_1) > threshold);
+  // onLine_r1 = (analogRead(IR_PIN_RIGHT_1) > threshold);
   onLine_r2 = (analogRead(IR_PIN_RIGHT_2) > threshold);
-  onLine_l1 = (analogRead(IR_PIN_LEFT_1) > threshold);
+  // onLine_l1 = (analogRead(IR_PIN_LEFT_1) > threshold);
   onLine_l2 = (analogRead(IR_PIN_LEFT_2) > threshold);
+  onLine_center = (analogRead(CENTER) > threshold);
 
   // Serial.print("  -  IR_PIN_RIGHT_1: ");
   // Serial.print(analogRead(IR_PIN_RIGHT_1));
